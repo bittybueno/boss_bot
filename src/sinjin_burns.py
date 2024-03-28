d275c7bad800e78ef346740ad6c499c54581a7ba
Marissa Bueno <marissa.bueno@datadoghq.com>
2024-03-28 14:05:39 -0600

    more burns

--- src/sinjin_burns.py
+++ src/sinjin_burns.py
@@ -2,5 +2,7 @@ sinjin_burns = [
     "Sinjin, did you break staging again?",
     "Sinjin, did you skip the learning session again?",
     "Sinjin, did you make sure to enable the new crawlers before disabling the old ones?",
-    "Sinjin, did you make _another_ backlog ticket?"
+    "Sinjin, did you make _another_ backlog ticket?",
+    "I've noticed that you've been leaving at 5:00pm on the dot every day this week. Is there something you'd like to share with the team?",
+    "Sinjin, truthfully, those slides were a bit of a mess. I think you could have done better.",
 ]
